35962f7f661bd1cd9d10371552633de32b79e73b
Lebranc Convas <33986491+lebrancconvas@users.noreply.github.com>
2018-11-15 02:32:32 +0700

    Edit Async Example.

--- async.js
+++ async.js
@@ -1,26 +1,13 @@
-function fnWorkFirst() {
-    console.log("Work First")
-    // callback()
+async function SetTime() {
+    let aTime = await setTimeout(() => {
+        console.log('a')
+    },1000)
+    let bTime = await setTimeout(() => {
+        console.log('b')
+    }, 100)
+    let cTime = setTimeout(() => {
+        console.log('c')
+    }, 2000)
 }
 
-function fnWorkSecond() {
-    setTimeout(() => {
-        console.log("Work Second")
-        // callback()
-    }, 1000)
-}
-
-function fnWorkThird() {
-    setTimeout(() => {
-        console.log("Work Third")
-        // callback()
-    }, 100);
-}
-
-async function mainWork() {
-    let a = await fnWorkFirst()
-    let b = fnWorkSecond()
-    let c = await fnWorkThird()
-}
-
-mainWork()
+SetTime()
