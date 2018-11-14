d05da22b41739afd95627a62d0b6735b7af9223c
Lebranc Convas <33986491+lebrancconvas@users.noreply.github.com>
2018-11-15 03:50:21 +0700

    [2] Edit Callback Example.

--- callback.js
+++ callback.js
@@ -1,13 +1,28 @@
-function SetTime() {
-    setTimeout(() => {
-		console.log('a')
-		setTimeout(() => {
-			console.log('b')
-			setTimeout(() => {
-				console.log('c')
-			},1000)
-		},100)
-	},2000)
+function wait3(callback) {
+	setTimeout(() => {
+        console.log('3 Second')
+        callback()
+    },3000)
 }
 
-SetTime()
+function wait5() {
+    setTimeout(() => {
+        console.log('5 Second')
+    }, 5000)
+}
+
+function instance4(callback) {
+    console.log('4 Second but Instance')
+    callback()
+}
+
+function wait() {
+    wait3(function() {
+        instance4(function() {
+            wait5()
+        })
+    })
+}
+
+wait()
+
